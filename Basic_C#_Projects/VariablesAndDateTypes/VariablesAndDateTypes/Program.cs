4cf84b931fa8c874e58a967a77fc2ca1e740fbc7
Keefer184 <136768491+Keefer184@users.noreply.github.com>
2023-07-06 13:38:44 -0400

    completing challenges and Math and Comparison Operators Assignment

--- Basic_C#_Projects/VariablesAndDateTypes/VariablesAndDateTypes/Program.cs
+++ Basic_C#_Projects/VariablesAndDateTypes/VariablesAndDateTypes/Program.cs
@@ -23,27 +23,48 @@ namespace VariablesAndDateTypes
             //Console.WriteLine("Your favorite number plus 5 is: " + total);
             //Console.ReadLine();
 
-            bool isStudying = false;
-            byte hoursWorked = 42;
-            sbyte currentTemperature = -23;
-            char questionMark = '\u2103';
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemperature = -23;
+            //char questionMark = '\u2103';
 
-            decimal moneyInBank = 100.5m;
+            //decimal moneyInBank = 100.5m;
 
-            double heightInCm = 2112.302302092;
+            //double heightInCm = 2112.302302092;
 
-            float secondsLeft = 2.62f;
+            //float secondsLeft = 2.62f;
 
-            short tempartureOnMars = -341;
+            //short tempartureOnMars = -341;
 
-            int currentAge = 30;
-            string yearsOld = currentAge.ToString();
+            //int currentAge = 30;
+            //string yearsOld = currentAge.ToString();
 
-            bool isRaining = true;
-            string rainingStatus = Convert.ToString(isRaining);
+            //bool isRaining = true;
+            //string rainingStatus = Convert.ToString(isRaining);
 
-            Console.WriteLine(rainingStatus);
+            //Console.WriteLine(rainingStatus);
+            //Console.ReadLine();
+
+            //Math Challenge
+            decimal x = 5;
+            short y = 7;
+            decimal totalAdd = x + y;
+            decimal totalSubt = x - y;
+            decimal totalMult = x * y;
+            decimal divInt = x / y;
+            //decimal totalDiv = Convert.ToDecimal(divInt);
+            decimal totalRemainder = x % y;
+            Console.WriteLine("x and y added together = " + totalAdd);
             Console.ReadLine();
+            Console.WriteLine("x subtract y = " + totalSubt);
+            Console.ReadLine();
+            Console.WriteLine("x multiplied by y = " + totalMult);
+            Console.ReadLine();
+            Console.WriteLine("x divided by y = " + divInt);
+            Console.ReadLine();
+            Console.WriteLine("Remainder of x divided by y = " + totalRemainder);
+            Console.ReadLine();
+
         }
     }
 }
