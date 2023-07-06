9ffa3603b3bd23702cd95a8fe005f2b178c52fd0
Keefer184 <136768491+Keefer184@users.noreply.github.com>
2023-07-05 21:34:39 -0400

    109 Steps into C# and Unity Course

---
+++ Basic_C#_Projects/VariablesAndDateTypes/VariablesAndDateTypes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDateTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadRine();
+            //Console.WriteLine("your name is: " + yourname);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCm = 2112.302302092;
+
+            float secondsLeft = 2.62f;
+
+            short tempartureOnMars = -341;
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+        }
+    }
+}
