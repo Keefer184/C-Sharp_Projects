78a9740ef5490e0de0b47ed250c8c76efb07df65
Keefer184 <136768491+Keefer184@users.noreply.github.com>
2023-07-06 23:06:43 -0400

    Arrays Submission Assignment added plus challenges and video notes updated

--- Basic_C#_Projects/ChallengeWorkspace/ChallengeWorkspace/Program.cs
+++ Basic_C#_Projects/ChallengeWorkspace/ChallengeWorkspace/Program.cs
@@ -80,23 +80,41 @@ namespace ChallengeWorkspace
             //    Console.ReadLine();
 
 
-            Console.WriteLine("Please pick a fruit choice by entering the letter of your choice:\nA.) Apple\nB.) Banana\nC.) Cantelope\nD.) Dragon Fruit");
-            string choice = Console.ReadLine();
-            switch (choice)
-            {
-                case "A":
-                    Console.WriteLine("Applese are the best.");
-                    break;
-                case "B":
-                    Console.WriteLine("Bananas are ok... a bit to phallic shaped for my taste... but hey man, you do you...");
-                    break;
-                case "C":
-                    Console.WriteLine("I like cantelope too, but they are often out of season, or not ripe enough... \nbut a good cantelope beats an apple any day!");
-                    break;
-                case "D":
-                    Console.WriteLine("I\'ve never had dragon fruit before.  Looks a little sus though...");
-                    break;
-            }
+            //Console.WriteLine("Please pick a fruit choice by entering the letter of your choice:\nA.) Apple\nB.) Banana\nC.) Cantelope\nD.) Dragon Fruit");
+            //string choice = Console.ReadLine();
+            //switch (choice)
+            //{
+            //    case "A":
+            //        Console.WriteLine("Applese are the best.");
+            //        break;
+            //    case "B":
+            //        Console.WriteLine("Bananas are ok... a bit to phallic shaped for my taste... but hey man, you do you...");
+            //        break;
+            //    case "C":
+            //        Console.WriteLine("I like cantelope too, but they are often out of season, or not ripe enough... \nbut a good cantelope beats an apple any day!");
+            //        break;
+            //    case "D":
+            //        Console.WriteLine("I\'ve never had dragon fruit before.  Looks a little sus though...");
+            //        break;
+            //}
+            //Console.ReadLine();
+
+            string greeting = "Hello, ";
+            string name = "Matt";
+            name = name.ToUpper();
+            string pleasentry = ". How are you today?";
+            string welcomeMessage = greeting + name + pleasentry;
+
+            Console.WriteLine(welcomeMessage);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("There once was a ship that put to sea");
+            sb.Append("\nAnd the name of this ship was the Bully O\'Tea.");
+            sb.Append("\nThe winds blew up, her bow dipped down");
+            sb.Append("\nOh blow my bully boys blow...");
+
+            Console.WriteLine(sb);
             Console.ReadLine();
         }
     }
