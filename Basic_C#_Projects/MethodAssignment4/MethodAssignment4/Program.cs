3e5acd4e17cf2eb4b6e969098da862f61a28a4d3
Keefer184 <136768491+Keefer184@users.noreply.github.com>
2023-07-08 16:02:41 -0400

    method assignment 4

---
+++ Basic_C#_Projects/MethodAssignment4/MethodAssignment4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Voidpractice data = new Voidpractice();
+            data.Nonsense(5, 12);
+            int yup = 7;
+            int huh = 9;
+            data.Nonsense(yup, huh);
+            Console.ReadLine();
+
+        }
+    }
+}
