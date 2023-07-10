fe6a90885f84a9f7a59d30c90206975ce44412e7
Keefer184 <136768491+Keefer184@users.noreply.github.com>
2023-07-10 19:06:58 -0400

    added parameters assignment

---
+++ Basic_C#_Projects/ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    public class Employee<T>: Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+        public int Id { get; set; }
+        public List<T> things { get; set; }
+
+    }
+}
---
+++ Basic_C#_Projects/ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> things1 = new List<string>() { "Wears glasses", "has brown hair", "likes to play pool" };
+            List<int> things2 = new List<int>() { 234, 985, 184, 2710 };
+            Employee<string> employee = new Employee<string>() { things = things1 };
+            Employee<int> employeeNums = new Employee<int>() { things = things2 };
+            foreach(string thing in employee.things)
+            {
+                Console.WriteLine(thing);
+            }
+            foreach(int num in employeeNums.things)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
