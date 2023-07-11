f4c708a125b4904a1ddd49837e7472406ee9a44c
Keefer184 <136768491+Keefer184@users.noreply.github.com>
2023-07-10 21:09:58 -0400

    struct challenge

---
+++ Basic_C#_Projects/StructChallenge/StructChallenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 3.1415927m;
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+
+
+        }
+    }
+}
