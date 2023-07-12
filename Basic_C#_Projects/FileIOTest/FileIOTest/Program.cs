390ac1cb09a589afab45c70e093d65b8abf5623b
Keefer184 <136768491+Keefer184@users.noreply.github.com>
2023-07-11 20:09:41 -0400

    Twenty One finish and File IO and DateTime add

---
+++ Basic_C#_Projects/FileIOTest/FileIOTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            DateTime current = Convert.ToDateTime(DateTime.Now);
+            DateTime future = current.AddHours(num);
+            Console.WriteLine("In exactly {0} hours it will be {1}", num, future);
+
+            Console.ReadLine();
+        }
+    }
+}
